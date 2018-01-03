b73d4c8530c71afb88cb3422bb914bdd3e8300f0
krisztian <alt.krisztian@gmail.com>
2018-01-03 20:21:00 +0100

    Hotfix: change GenerationType.AUTO to GenerationType.IDENTITY in ProductReview class

--- src/main/java/productreview/model/ProductReview.java
+++ src/main/java/productreview/model/ProductReview.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class ProductReview {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "userid")
--- src/main/resources/data-productreview.sql
+++ src/main/resources/data-productreview.sql
@@ -1,20 +1,17 @@
 
-DROP SEQUENCE public.hibernate_sequence;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
 
-CREATE SEQUENCE public.hibernate_sequence
-  INCREMENT 1
-  MINVALUE 1
-  MAXVALUE 9223372036854775807
-  START 5
-  CACHE 1;
-ALTER TABLE public.hibernate_sequence
-  OWNER TO bencepal;
+SET search_path = public, pg_catalog;
 
 INSERT INTO productreview VALUES (1, 2, 5, 'I liked it.', 3);
 INSERT INTO productreview VALUES (2, 2, 3, 'I''m not.', 1);
 INSERT INTO productreview VALUES (3, 2, 4, 'I liked it33', 4);
 INSERT INTO productreview VALUES (4, 3, 4, 'I likssssssssssss', 4);
 
-
-
--- SELECT pg_catalog.setval('id_seq', 4, true);
+SELECT pg_catalog.setval('productreview_id_seq', 4, true);
