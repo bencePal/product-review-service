7388c0d8117dfcfb5ed3391911d39b5a019953ac
krisztian <alt.krisztian@gmail.com>
2018-01-04 16:57:19 +0100

    Delete unnecessary import

--- src/main/java/productreview/api/ProductReviewRest.java
+++ src/main/java/productreview/api/ProductReviewRest.java
@@ -3,7 +3,6 @@ package productreview.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import productreview.model.ProductReview;
 import productreview.repository.ProductReviewRepository;
