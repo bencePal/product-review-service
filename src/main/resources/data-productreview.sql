a422a52b97448f26c944bfc877210119e78e7c0f
bencePal <paal.bence@gmail.com>
2018-01-04 16:30:31 +0100

    change template title, modify review text

--- src/main/resources/data-productreview.sql
+++ src/main/resources/data-productreview.sql
@@ -9,9 +9,9 @@ SET row_security = off;
 
 SET search_path = public, pg_catalog;
 
-INSERT INTO productreview VALUES (1, 2, 5, 'I liked it.', 3);
+INSERT INTO productreview VALUES (1, 2, 4, 'I liked it.', 3);
 INSERT INTO productreview VALUES (2, 2, 1, 'Bence is more beautiful than this thing', 1);
-INSERT INTO productreview VALUES (3, 2, 4, 'Fonok for president', 4);
-INSERT INTO productreview VALUES (4, 3, 4, 'Impressive. Most impressive.', 4);
+INSERT INTO productreview VALUES (3, 2, 5, 'Fabulous', 4);
+INSERT INTO productreview VALUES (4, 2, 4, 'Impressive. Most impressive.', 4);
 
 SELECT pg_catalog.setval('productreview_id_seq', 4, true);
